9bec18b9c7eb61cf4c21df283448cb6005b524e6
anilajoseph44 <anilasandrajoseph@gmail.com>
2024-02-09 16:18:58 +0530

    done

--- controllers/userroute.js
+++ controllers/userroute.js
@@ -1,17 +1,36 @@
 const express=require("express")
 const usermodel=require("../models/usermodel")
 const router=express.Router()
+const bcrypt=require("bcryptjs")
+
+hashpasswordgenerator=async(pswd)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pswd,salt)
+
+}
 
 router.post("/add",async(req,res)=>{
 
-    let data=req.body
-    let user=new usermodel(data)
-    let result=await user.save()
-    res.json(
-        {
-            status:"success"
+    let {data}={"data":req.body}
+    let password=data.password
+
+    hashpasswordgenerator(password).then(
+        (hashedpassword)=>{
+            console.log(hashedpassword)
+            data.password=hashedpassword
+            console.log(data)
+            let user=new usermodel(data)
+            let result=user.save()
+            res.json(
+                {
+                    status:"success"
+                }
+            )
+            
         }
     )
+
+    
 
 })
 
